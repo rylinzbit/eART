2a5502b683d9f0092acc77e3a5580b03f842ff1a
Holly Zbitowsky <hzbitowsky@yahoo.com>
2015-11-27 12:46:49 -0800

    correcting registration forms

--- application/controllers/artistLoginController.php
+++ application/controllers/artistLoginController.php
@@ -4,7 +4,7 @@ class ArtistLoginController extends CI_Controller {
 	
 	public function index()
 	{
-		$this->load->view('artworkPage');
+		$this->load->view('artistLogin');
 	}
 
 	public function artist_login()
@@ -22,12 +22,39 @@ class ArtistLoginController extends CI_Controller {
 			}
 			else{
 				$this->session->set_flashdata("error", "Invalid Log In");
-				redirect("artist_login");
+				redirect("ArtistLoginController");
 			}
 		}
 		else{
 			$this->session->set_flashdata("error", "Please enter Email and Password");
-			redirect("artist_login");
+			redirect("ArtistLoginController");
+		}
+	}
+
+	public function artist_register()
+	{
+		$this->load->library("form_validation");
+		$this->load->model("artist");
+
+		$this->form_validation->set_rules("first_name", "First Name", "trim|required|alpha");
+		$this->form_validation->set_rules("last_name", "Last Name", "trim|required|alpha");
+		$this->form_validation->set_rules("email", "Email Address", "trim|required|vaild_email|is_unique[artists.email]");
+		$this->form_validation->set_rules("password", "Password", "trim|required|min_length[8]|matches[confirm_password]");
+		$this->form_validation->set_rules("confirm_password", "Confirm Password", "trim|required");
+
+		if($this->form_validation->run()){
+			$first_name = $this->input->post("first_name");
+			$last_name = $this->input->post("last_name");
+			$email = $this->input->post("email");
+			$password = $this->input->post("password");
+
+			$this->session->set_flashdata("success", "Registration Successful!");
+			$this->artist->artist_register($first_name, $last_name, $email, $password);
+			redirect("ArtistLoginController");
+		}
+		else{
+			$this->session->set_flashdata("reg_errors", array(validation_errors()));
+			redirect("ArtistLoginController");
 		}
 	}
 }
